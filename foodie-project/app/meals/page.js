1d2dbdb8ac9fd469fcdc1e3392c4756c079f0495
junsu930 <bobdylan0331@gmail.com>
2024-03-25 15:25:56 +0900

    nextjs error handling and loading page

--- foodie-project/app/meals/page.js
+++ foodie-project/app/meals/page.js
@@ -1,15 +1,37 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <p>
-        <Link href="/meals/share">share page</Link>
-      </p>
-      <p>
-        <Link href="/">main page</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{''}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourselt. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe!</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
