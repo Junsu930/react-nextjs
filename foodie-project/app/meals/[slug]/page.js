f92d1d254a23844a7fa320450f0f32d513301229
junsu930 <bobdylan0331@gmail.com>
2024-03-28 19:35:51 +0900

    error handling

--- foodie-project/app/meals/[slug]/page.js
+++ foodie-project/app/meals/[slug]/page.js
@@ -1,13 +1,39 @@
-import Link from 'next/link';
+import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+import { notFound } from 'next/navigation';
 
-export default function EachMealPage({ params }) {
+export default function MealDetailPage({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+
   return (
-    <main>
-      <h1>Each Meal Page</h1>
-      <p>{params.slug}</p>
-      <p>
-        <Link href="/meals">meals page</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email} `}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
